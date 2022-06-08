50c24a35f0937040f9244dd5d2716d19cf2bce7e
CSCMWS\acolella <alesandro.colella@dxc.com>
2022-06-08 09:52:21 +0200

    changed ticket number validation regex

--- src/helpers/validations.tsx
+++ src/helpers/validations.tsx
@@ -1,6 +1,6 @@
 
 export const regex = {
-    ALPHA_NUMERIC_WITHOUT_SPECIAL_CHAR_PATTERN: /^[a-zA-Z0-9 .-]+$/,
+    ALPHA_NUMERIC_WITHOUT_SPECIAL_CHAR_PATTERN: /^[a-zA-Z0-9-]+$/,
     FISCAL_CODE_PATTERN: /^([A-Za-z]{6}[0-9lmnpqrstuvLMNPQRSTUV]{2}[abcdehlmprstABCDEHLMPRST]{1}[0-9lmnpqrstuvLMNPQRSTUV]{2}[A-Za-z]{1}[0-9lmnpqrstuvLMNPQRSTUV]{3}[A-Za-z]{1})|([0-9]{11})$/, 
     UNIQUE_IDENTIFIER_PATTERN: /^[A-Za-z0-9\~\-\_]*$/
 }
