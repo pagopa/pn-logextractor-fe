ead7e94ba442677cfb0bc1d6adfaa407b6bc0d07
CSCMWS\acolella <alesandro.colella@dxc.com>
2022-06-13 11:30:40 +0200

    canged header ui

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -6,7 +6,8 @@ import { Button, Container, Dialog, DialogActions, DialogContent, DialogContentT
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { infoMessages } from "../helpers/messagesConstants"
-import { Grid, Typography } from '@material-ui/core';
+import { Divider, Grid, Typography } from '@material-ui/core';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 /**
  * General component presenting the header of the app.
@@ -42,52 +43,49 @@ const Header = () => {
   }
 
   return (
-      <AppBar position="static">
-        <Toolbar>
-          <Grid container spacing={2} justifyContent="flex-end" alignItems="center">
-            <Grid item>
-              <Typography>test@test.com</Typography>
-            </Grid>
-            <Grid item>
-              <Tooltip title="Log out">
-                <IconButton
-                  size="large"
-                  edge="start"
-                  color="inherit"
-                  aria-label="menu"
-                  sx={{ mr: 2 }}
-                  onClick={handleOpenModal}
-                >
-                  <LogoutIcon sx={{ color: "white" }} />
-                </IconButton>
-              </Tooltip>
-            </Grid>
+    <AppBar position="static">
+      <Container>
+        <Toolbar sx={{paddingRight:"0px", '@media (min-width: 640px)':{paddingRight:"0px"}}}>
+          <Grid container justifyContent="flex-end" alignItems="center">
+            <AccountCircleIcon />
+            <Typography>test@test.com</Typography>
+            <Divider style={{ background: 'white' }} orientation="vertical" variant="middle" flexItem />
+            <Tooltip title="Log out">
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                onClick={handleOpenModal}
+              >
+                <LogoutIcon sx={{ color: "white" }} />
+              </IconButton>
+            </Tooltip>
           </Grid>
         </Toolbar>
-
-
-        <Dialog
-          open={open}
-          onClose={handleCloseModal}
-          aria-labelledby="alert-dialog-title"
-          aria-describedby="alert-dialog-description"
-        >
-          <DialogTitle id="alert-dialog-title">
-            LOG OUT
-          </DialogTitle>
-          <DialogContent>
-            <DialogContentText id="alert-dialog-description">
-              {infoMessages.LOGOUT_CONFIRMATION}
-            </DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button onClick={handleCloseModal}>Annulla</Button>
-            <Button onClick={handleLogOut} autoFocus>
-              Esci
-            </Button>
-          </DialogActions>
-        </Dialog>
-      </AppBar >
+      </Container>
+      <Dialog
+        open={open}
+        onClose={handleCloseModal}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          LOG OUT
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            {infoMessages.LOGOUT_CONFIRMATION}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseModal}>Annulla</Button>
+          <Button onClick={handleLogOut} autoFocus>
+            Esci
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </AppBar >
   )
 }
 
