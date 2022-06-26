fca090db2948113948314eccb357190ff0c9f258
Emilia Petkova <epetkova2@dxc.com>
2022-06-26 15:37:16 +0300

    add api tests

--- src/__tests__/api.test.tsx
+++ src/__tests__/api.test.tsx
@@ -31,6 +31,15 @@ describe('Api requests tests', () => {
         expect(data).toMatchObject({"data": expect.any(String)})
     });
 
+    it('getPersonId with error', async() => {
+        const payload = {
+            recipientType: "PG",
+            ticketNumber: "abc",
+        }
+        const request = apiRequests.getPersonId(payload as getPersonIdType)
+        expect(await request).toThrowError()
+    });
+
     it('getPersonTaxId', async() => {
         const payload: getPersonTaxIdType = {
             personId: "abc"
@@ -38,6 +47,14 @@ describe('Api requests tests', () => {
         const request = apiRequests.getPersonTaxId(payload)
         const {data} = await request;
         expect(data).toMatchObject({"data": expect.any(String)})
+    });
+
+    it('getPersonTaxId with error', async() => {
+        const payload = {
+            personId: "./+"
+        }
+        const request = apiRequests.getPersonTaxId(payload as getPersonTaxIdType)
+        expect(await request).toThrowError()
     });
 
     it('getNotificationsInfoLogs', async() => {
@@ -48,6 +65,14 @@ describe('Api requests tests', () => {
         const request = apiRequests.getNotificationsInfoLogs(payload)
         const res = await request;
         expect(res).toMatchObject({"data": expect.any(String)})
+    });
+
+    it('getNotificationsInfoLogs with error', async() => {
+        const payload = {
+            ticketNumber: "abc",
+        }
+        const request = apiRequests.getNotificationsInfoLogs(payload as getNotificationsInfoLogsType)
+        expect(await request).toThrowError();
     });
 
     it('getNotificationsMonthlyStatsLogs', async() => {
@@ -61,6 +86,15 @@ describe('Api requests tests', () => {
         expect(res).toMatchObject({"data": expect.any(String)})
     });
 
+    it('getNotificationsMonthlyStatsLogs with error', async() => {
+        const payload:getNotificationsMonthlyStatsLogsType = {
+            ticketNumber: "abc",
+            ipaCode: 0,
+        }
+        const request = apiRequests.getNotificationsMonthlyStatsLogs(payload)
+        expect(await request).toFa();
+    });
+
     it('getLogsProcesses', async() => {
         const payload:getLogsProcessesType = {
             traceId: "Root=1-628cd739-3a11653723d704c80fa4060e",
@@ -70,6 +104,15 @@ describe('Api requests tests', () => {
         const request = apiRequests.getLogsProcesses(payload)
         const res = await request;
         expect(res).toMatchObject({"data": expect.any(String)})
+    });
+
+    it('getLogsProcesses with errors', async() => {
+        const payload = {
+            traceId: "Root=1-628cd739-3a11653723d704c80fa4060e",
+            dateFrom: "2022-03-25",
+        }
+        const request = apiRequests.getLogsProcesses(payload as getLogsProcessesType)
+        expect(await request).toThrowError();
     });
 
     it('getPersonsLogs', async() => {
@@ -83,6 +126,15 @@ describe('Api requests tests', () => {
         expect(res).toMatchObject({"data": expect.any(String)})
     });
 
+    it('getPersonsLogs with errors', async() => {
+        const payload = {
+            ticketNumber: "abc",
+            deanonimization: false,
+        }
+        const request = apiRequests.getPersonsLogs(payload as getPersonsLogsType)
+        expect(await request).toThrowError();
+    });
+
     it('getOperatorsLogs', async() => {
         const payload:getOperatorsLogsType = {
             ticketNumber: "abc",
@@ -93,5 +145,15 @@ describe('Api requests tests', () => {
         const request = apiRequests.getOperatorsLogs(payload)
         const res = await request;
         expect(res).toMatchObject({"data": expect.any(String)})
+    });
+
+    it('getOperatorsLogs with errors', async() => {
+        const payload = {
+            ticketNumber: "abc",
+            taxId: "MLLSNT82P65Z404U",
+            dateTo: "2022-06-15"
+        }
+        const request = apiRequests.getOperatorsLogs(payload as getOperatorsLogsType)
+        expect(await request).toThrowError();
     });
 });
